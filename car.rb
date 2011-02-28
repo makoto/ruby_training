12c62f406b1e179ebdf88a3fc90fdf07d30a366a
Makoto Inoue <inouemak@googlemail.com>
2011-02-28 11:15:25 +0000

    Replaced setter/getter to attr_accessor

--- car.rb
+++ car.rb
@@ -1,14 +1,7 @@
 class Car
+  attr_accessor :top_speed
   def initialize(top_speed = 100)
     @top_speed = top_speed
-  end
-  
-  def top_speed
-    @top_speed
-  end
-  
-  def top_speed=(speed)
-    @top_speed = speed
   end
 end
 
